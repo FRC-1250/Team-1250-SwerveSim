2fe5d1fe1d2361a4e92b0c454d931d9f0d225d7b
1250-tars <1250robotics+programming@gmail.com>
2022-11-17 20:45:08 -0500

    Working 1 module implementation
    - 1 module setup with talons and cancoder running on a canivore (Party Bus)
    - Swerve module movement based on position and velocity
    
    Co-Authored-By: Astronel <78625133+Astronel@users.noreply.github.com>
    Co-Authored-By: Canibal_Burger <78625342+canibalburger@users.noreply.github.com>

--- src/main/java/frc/robot/SwerveModuleTalonFX.java
+++ src/main/java/frc/robot/SwerveModuleTalonFX.java
@@ -12,32 +12,38 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModuleTalonFX {
+    private static final int METERS_PER_SECOND_TO_TALON_TICKS_CONVERSION_FACTOR = 2000;
+    private static final double DEGRESS_TO_TALON_TICKS_CONVERSION_FACTOR = 11.4;
     private final WPI_TalonFX driveTalon;
     private final WPI_TalonFX turningTalon;
     private final CANCoder canCoder;
 
     public SwerveModuleTalonFX(int driveTalonCanID, int turningTalonCanID, int canCoderCanID) {
-        driveTalon = new WPI_TalonFX(driveTalonCanID);
+        driveTalon = new WPI_TalonFX(driveTalonCanID, Constants.PARTY_BUS);
         driveTalon.configFactoryDefault();
         driveTalon.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.CAN_TIMEOUT_MS);
         driveTalon.configClosedloopRamp(0.5);
+        driveTalon.configPeakOutputForward(0.5);
+        driveTalon.configPeakOutputReverse(-0.5);
         driveTalon.config_kF(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_VELOCITY_GAINS.kF, Constants.CAN_TIMEOUT_MS);
         driveTalon.config_kP(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_VELOCITY_GAINS.kP, Constants.CAN_TIMEOUT_MS);
         driveTalon.config_kI(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_VELOCITY_GAINS.kI, Constants.CAN_TIMEOUT_MS);
         driveTalon.config_kD(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_VELOCITY_GAINS.kD, Constants.CAN_TIMEOUT_MS);
 
-        canCoder = new CANCoder(canCoderCanID);
+        canCoder = new CANCoder(canCoderCanID, Constants.PARTY_BUS);
         canCoder.configFactoryDefault();
         canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);
-        canCoder.configSensorDirection(true);
+        canCoder.configSensorDirection(false);
         canCoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
 
-        turningTalon = new WPI_TalonFX(turningTalonCanID);
+        turningTalon = new WPI_TalonFX(turningTalonCanID, Constants.PARTY_BUS);
         turningTalon.configFactoryDefault();
-
         turningTalon.configRemoteFeedbackFilter(canCoder, 0);
         turningTalon.configSelectedFeedbackSensor(RemoteFeedbackDevice.RemoteSensor0, Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.CAN_TIMEOUT_MS);
         turningTalon.configIntegratedSensorAbsoluteRange(AbsoluteSensorRange.Signed_PlusMinus180);
+        turningTalon.configClosedloopRamp(0.5);
+        turningTalon.configPeakOutputForward(0.5);
+        turningTalon.configPeakOutputReverse(-0.5);
         turningTalon.config_kF(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_POSITION_GAINS.kF, Constants.CAN_TIMEOUT_MS);
         turningTalon.config_kP(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_POSITION_GAINS.kP, Constants.CAN_TIMEOUT_MS);
         turningTalon.config_kI(Constants.TALONFX_PRIMARY_PID_LOOP_ID, Constants.TALONFX_POSITION_GAINS.kI, Constants.CAN_TIMEOUT_MS);
@@ -50,8 +56,8 @@ public class SwerveModuleTalonFX {
 
     public void setDesiredState(SwerveModuleState desiredState) {
         SwerveModuleState state = SwerveModuleState.optimize(desiredState, getFromHeading());
-        driveTalon.set(ControlMode.Velocity, state.speedMetersPerSecond);
-        turningTalon.set(ControlMode.Position, state.angle.getDegrees());
+        driveTalon.set(ControlMode.Velocity, state.speedMetersPerSecond * METERS_PER_SECOND_TO_TALON_TICKS_CONVERSION_FACTOR);
+        turningTalon.set(ControlMode.Position, state.angle.getDegrees() * DEGRESS_TO_TALON_TICKS_CONVERSION_FACTOR);
     }
 
     private Rotation2d getFromHeading() {
